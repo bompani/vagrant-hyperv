04f0e48108cd37bc551ecfc9a2f8407316332178
Luca Bompani <luca.bompani@unibo.it>
2023-03-03 17:34:40 +0100

    network trigger

--- bin/configure-static-ip.sh
+++ bin/configure-static-ip.sh
@@ -1,4 +1,52 @@
-#!/bin/sh
+#!/bin/bash
+
+MACADDRESS=""
+IP=""
+NETMASK="" 
+
+usage() {
+  echo "Usage: $0  -m MAC-Address -i IP-Address -n netmask" 1>&2 
+}
+
+exit_abnormal() {
+  usage
+  exit 1
+}
+
+while getopts "m:i:n:" options; do 
+  case "${options}" in
+    m)
+      MACADDRESS=${OPTARG}
+      ;;
+    i)
+      IP=${OPTARG}
+      ;;
+    n)
+      NETMASK=${OPTARG}
+      ;;
+    :)
+      echo "Error: -${OPTARG} requires an argument."
+      exit_abnormal
+      ;;
+    *)
+      exit_abnormal
+      ;;
+  esac
+done
+
+if [ "$MACADDRESS" = "" ] || [ "$IP" = "" ] || [ "$NETMASK" = "" ]; then
+  echo "Error: missing required argument."
+  exit_abnormal
+fi
+
+interfaceMAC=$(echo "$MACADDRESS" | sed -r 's/-/:/g' | tr '[:upper:]' '[:lower:]')
+interface=$(ip -br link | awk "\$3 ~ /$interfaceMAC/ {print \$1}")
+if [ -z "$interface" ]
+then
+  echo "Did not find interface"
+  exit 1
+fi
+
 . /etc/os-release
 
 echo 'Setting static IP address for Hyper-V... '
@@ -6,29 +54,25 @@ echo 'Setting static IP address for Hyper-V... '
 case $ID_LIKE in
 
   *debian*)
-cat << EOF > /etc/netplan/01-netcfg.yaml
+cat << EOF > /etc/netplan/01-netcfg-$interface.yaml
 network:
   version: 2
   ethernets:
-    eth0:
+    $interface:
       dhcp4: no
-      addresses: [$1/24]
-      gateway4: 10.42.100.1
-      nameservers:
-        addresses: [137.204.25.71,137.204.25.213,137.204.25.77]
+      addresses: [$IP/$NETMASK]
 EOF
     ;;
   *rhel*)
-cat << EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
-DEVICE=eth0
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$interface
+DEVICE=$interface
 BOOTPROTO=none
 ONBOOT=yes
-PREFIX=24
-IPADDR=$1
-GATEWAY=10.42.100.1
-DNS1=137.204.25.213
-DNS2=8.8.4.4
-DNS3=137.204.25.77
+PREFIX=$NETMASK
+IPADDR=$IP
 EOF
     ;;
 esac
+
+
+exit 0
