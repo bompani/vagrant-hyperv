e36431daf1b2cad39859fd52698c2fd918e1382f
Luca Bompani <luca.bompani@unibo.it>
2023-03-20 20:55:53 +0100

    ssh key

--- Vagrantfile
+++ Vagrantfile
@@ -16,6 +16,12 @@ windowsNet3 = {}
 windowsNet3["ip"] = "10.42.203.101"
 windowsNet3["netmask"] = "255.255.255.0"
 windows["network"].push(windowsNet3)
+windows["synced_folder"] = []
+windowsSyncFolder1 = {}
+windowsSyncFolder1["src"] = "."
+windowsSyncFolder1["dst"] = "/c/vagrant"
+windowsSyncFolder1["exclude"] = ".git/"
+windows["synced_folder"].push(windowsSyncFolder1)
 vms.push(windows)
 
 ubuntu = {}
@@ -34,6 +40,12 @@ ubuntuNet3 = {}
 ubuntuNet3["ip"] = "10.42.203.102"
 ubuntuNet3["netmask"] = "255.255.255.0"
 ubuntu["network"].push(ubuntuNet3)
+ubuntu["synced_folder"] = []
+ubuntuSyncFolder1 = {}
+ubuntuSyncFolder1["src"] = "."
+ubuntuSyncFolder1["dst"] = "/vagrant"
+ubuntuSyncFolder1["exclude"] = ".git/"
+ubuntu["synced_folder"].push(ubuntuSyncFolder1)
 vms.push(ubuntu)
 
 centos = {}
@@ -52,17 +64,27 @@ centosNet3 = {}
 centosNet3["ip"] = "10.42.203.103"
 centosNet3["netmask"] = "255.255.255.0"
 centos["network"].push(centosNet3)
+centos["synced_folder"] = []
+centosSyncFolder1 = {}
+centosSyncFolder1["src"] = "."
+centosSyncFolder1["dst"] = "/vagrant"
+centosSyncFolder1["exclude"] = ".git/"
+centos["synced_folder"].push(centosSyncFolder1)
 vms.push(centos)
 
 Vagrant.configure("2") do |config|
   vms.each do |node|
     config.vm.define node["hostname"] do |node_config|    
       node_config.vm.box = node["box"]
-      node_config.vm.hostname = node["hostname"]   
+      node_config.vm.hostname = node["hostname"] 
+
       node["network"].each do |network|
         node_config.vm.network :private_network, ip: network["ip"], netmask: network["netmask"]
       end
-      node_config.vm.synced_folder ".", "/vagrant", type: "rsync", rsync__exclude: ".git/"
+
+      node["synced_folder"].each do |folder|
+        node_config.vm.synced_folder folder["src"], folder["dst"], type: "rsync", rsync__exclude: folder["exclude"]
+      end
 
       node_config.vm.provider "hyperv" do |p|
         p.linked_clone = true
--- lib/vagrant-hypervnet/cap/windows/sshd.rb
+++ lib/vagrant-hypervnet/cap/windows/sshd.rb
@@ -1,3 +1,6 @@
+
+require 'vagrant/util/keypair'
+
 module VagrantPlugins
   module HyperVNet
     module Cap
@@ -11,16 +14,52 @@ module VagrantPlugins
           def self.sshd_install(machine)
             machine.communicate.sudo("Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'",
               shell: :powershell, elevated: true)
+              
+            if machine.config.ssh.insert_key              
+              insert = false
+              ssh_info = machine.ssh_info
+              if !ssh_info.nil?
+                insert = ssh_info[:password] && ssh_info[:private_key_path].empty?
+                ssh_info[:private_key_path].each do |pk|
+                  if insecure_key?(pk)
+                    insert = true
+                    machine.ui.detail("\n"+I18n.t("vagrant_hypervnet.ssh.inserting_insecure_detected"))
+                    break
+                  end
+                end
+              end
 
-            ssh_key_url = "https://github.com/hashicorp/vagrant/raw/main/keys/vagrant.pub"
-            ssh_key_path = "C:\\ProgramData\\ssh\\administrators_authorized_keys"
+              if insert
+                _pub, priv, openssh = Vagrant::Util::Keypair.create  
+                machine.ui.detail("\n"+I18n.t("vagrant_hypervnet.ssh.inserting_random_key"))
+                insert_public_key(machine, openssh)
+      
+                # Write out the private key in the data dir so that the
+                # machine automatically picks it up.
+                machine.data_dir.join("private_key").open("w+") do |f|
+                  f.write(priv)
+                end
 
+                # Adjust private key file permissions if host provides capability
+                if machine.env.host.capability?(:set_ssh_key_permissions)
+                  machine.env.host.capability(:set_ssh_key_permissions, machine.data_dir.join("private_key"))
+                end
+              
+                # Remove the old key if it exists
+                machine.ui.detail(I18n.t("vagrant_hypervnet.ssh.inserting_remove_key"))
+                remove_public_key(machine, Vagrant.source_root.join("keys", "vagrant.pub").read.chomp)
+      
+                machine.ui.detail(I18n.t("vagrant_hypervnet.ssh.inserted_key")) 
+              end
+            end
+            
             cmds = []
             cmds += [
-              "Invoke-WebRequest -Uri '#{ssh_key_url}' -OutFile '#{ssh_key_path}'",
-              "icacls '#{ssh_key_path}' /inheritance:r /grant 'Administrators:F' /grant 'SYSTEM:F'",
-              "Start-Service -Name sshd",
-              "Set-Service -Name sshd -StartupType 'Automatic'"
+              '(Get-Content "$Env:ProgramData\ssh\sshd_config" | select-string -pattern ' +
+              '"Match Group administrators|AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys" -notmatch) | ' +
+              'Set-Content "$Env:ProgramData\ssh\sshd_config"',
+              'Start-Service -Name sshd',
+              'Set-Service -Name sshd -StartupType "Automatic"'
             ]
 
             machine.communicate.tap do |comm|
@@ -29,6 +68,78 @@ module VagrantPlugins
               end
             end
 
+          end
+
+          def self.insecure_key?(path)
+            return false if !path
+            return false if !File.file?(path)
+            source_path = Vagrant.source_root.join("keys", "vagrant")
+            return File.read(path).chomp == source_path.read.chomp
+          end
+
+          def self.insert_public_key(machine, contents)
+            contents = contents.strip
+            winssh_modify_authorized_keys machine do |keys|
+              if !keys.include?(contents)
+                keys << contents
+              end
+            end
+          end
+  
+          def self.remove_public_key(machine, contents)
+            winssh_modify_authorized_keys machine do |keys|
+              keys.delete(contents)
+            end
+          end
+  
+          def self.winssh_modify_authorized_keys(machine)
+            comm = machine.communicate
+            directories = fetch_guest_paths(comm)
+            home_dir = directories[:home]
+            temp_dir = directories[:temp]
+  
+            # Ensure the user's ssh directory exists
+            remote_ssh_dir = "#{home_dir}\\.ssh"
+            comm.execute("New-Item -Path '#{remote_ssh_dir}' -ItemType directory -Force", shell: "powershell")
+            remote_upload_path = "#{temp_dir}\\vagrant-insert-pubkey-#{Time.now.to_i}"
+            remote_authkeys_path = "#{remote_ssh_dir}\\authorized_keys"
+  
+            keys_file = Tempfile.new("vagrant-windows-insert-public-key")
+            keys_file.close
+            # Check if an authorized_keys file already exists
+            result = comm.execute("dir \"#{remote_authkeys_path}\"", shell: "cmd", error_check: false)
+            if result == 0
+              comm.download(remote_authkeys_path, keys_file.path)
+              keys = File.read(keys_file.path).split(/[\r\n]+/)
+            else
+              keys = []
+            end
+            yield keys
+            File.write(keys_file.path, keys.join("\r\n") + "\r\n")
+            comm.upload(keys_file.path, remote_upload_path)
+            keys_file.delete
+            comm.execute(<<-EOC.gsub(/^\s*/, ""), shell: "powershell")
+              Set-Acl "#{remote_upload_path}" (Get-Acl "#{remote_authkeys_path}")
+              Move-Item -Force "#{remote_upload_path}" "#{remote_authkeys_path}"
+            EOC
+          end
+  
+          # Fetch user's temporary and home directory paths from the Windows guest
+          #
+          # @param [Communicator]
+          # @return [Hash] {:temp, :home}
+          def self.fetch_guest_paths(communicator)
+            output = ""
+            communicator.execute("Write-Output $env:TEMP\nWrite-Output $env:USERPROFILE", shell: "powershell") do |type, data|
+              if type == :stdout
+                output << data
+              end
+            end
+            temp_dir, home_dir = output.strip.split(/[\r\n]+/)
+            if temp_dir.nil? || home_dir.nil?
+              raise Errors::PublicKeyDirectoryFailure
+            end
+            {temp: temp_dir, home: home_dir}
           end
         end
       end
