b0553b60c38324274aa4c481093645e02586ce1e
Luca Bompani <luca.bompani@unibo.it>
2023-04-04 22:13:21 +0200

    readme

--- lib/vagrant-hypervnet/cap/windows/sshd.rb
+++ lib/vagrant-hypervnet/cap/windows/sshd.rb
@@ -14,7 +14,7 @@ module VagrantPlugins
           def self.sshd_install(machine)
             machine.ui.detail(I18n.t("vagrant_hypervnet.ssh.install")) 
 
-            machine.communicate.sudo("Add-WindowsCapability -Online -Name 'OpenSSH.Server'",
+            machine.communicate.sudo("Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'",
               shell: :powershell, elevated: true)
               
             if machine.config.ssh.insert_key              
